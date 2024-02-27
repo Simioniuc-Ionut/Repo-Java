6cba527eaf757c4205deee8ae5836f5a368ae44e
Simioniuc Ionut <simioniucionut@gmail.com>
2024-02-27 19:42:52 +0200

    Lab1

--- Lab1/src/main/java/org/example/Main.java
+++ Lab1/src/main/java/org/example/Main.java
@@ -1,13 +1,12 @@
 import java.lang.reflect.Array;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        //Compulsory
+        //Ex1
         System.out.printf("Hello World!");
-
+        //Ex2
         String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
 
         int n = (int) (Math.random() * 1000000);
